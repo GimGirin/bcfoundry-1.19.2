74fd87260a31dbed1a264400562ed9bd75f1b53d
GimGirin <theodoreyjg@gmail.com>
2023-05-13 22:44:55 -0700

    Initial commit

---
+++ src/main/java/gim/bcfoundry/BCFClient.java
@@ -0,0 +1,10 @@
+package gim.bcfoundry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BCFClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
